06936f3b22f6e69a761b152db8a28facea128448
imchannnn <99274233+imchannnn@users.noreply.github.com>
2023-02-19 18:34:32 +0800

    cinemachine好了
    觸發到結束都做好了外加傷口動畫

--- WinterProject/Assets/Sword.cs
+++ WinterProject/Assets/Sword.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Cinemachine;
 
 public class Sword : MonoBehaviour
 {
-    private Animator animator;
+    
     public ParticleSystem ex;
+    public GameObject CM;
     void Start()
     {
-        animator = GetComponent<Animator>();
+        CM.transform.gameObject.SetActive(false);
+        //CM.GetComponent<CinemachineVirtualCamera>().enabled = false;
+        //this.GetComponent<CinemachineVirtualCamera>().enabled = false;
+        ex.Stop();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (ex.isStopped)
         {
+            CM.transform.gameObject.SetActive(true);
             ex.Play();
+            //this.GetComponent<CinemachineVirtualCamera>().enabled = true;
         }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        CM.transform.gameObject.SetActive(false);
+        //this.GetComponent<CinemachineVirtualCamera>().enabled = false;
     }
 }
